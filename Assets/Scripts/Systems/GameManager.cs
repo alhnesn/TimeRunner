31712a23b8c25b3ec27b88d103ed73f0321e7d63
Eylül Topçu <topcueyll@gmail.com>
2025-05-05 22:17:41 +0300

    jam final 1

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -3,6 +3,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject mainMenuUI;
+    public GameObject ingameUI;
+    public GameObject player;
+    public GameObject chaser;
+    public GameObject rocket;
+    private bool gameStarted=false;
+
+    public void Start()
+    {
+        
+        mainMenuUI.SetActive(true);
+        ingameUI.SetActive(false);
+        player.SetActive(false);
+        chaser.SetActive(false);
+        rocket.SetActive(false);
+
+
+    }
+
+    public void Update()
+    {
+        if (!gameStarted&&(Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.A)))
+{
+    gameStarted=true;
+        player.SetActive(true);
+        chaser.SetActive(true);
+        rocket.SetActive(true);
+        mainMenuUI.SetActive(false);
+        ingameUI.SetActive(true);
+
+
+}
+    }
     public void GameOver()
     {
         ScoreManagerTMP.I.OnPlayerDeath();
@@ -12,4 +45,8 @@ public class GameManager : MonoBehaviour
         // Buraya sahne reset, animasyon veya menü açma eklenebilir
         Time.timeScale = 0f;
     }
+
+
+
+    
 }
